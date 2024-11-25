9aa21be4cab82043a7642ae87aa32b6a00b46663
Sugarú <jaime01casanovam@gmail.com>
2024-11-24 22:18:23 -0400

    Arreglo error css no actualizado

--- pages/list_contacts.php
+++ pages/list_contacts.php
@@ -7,7 +7,7 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <!-- Estilos personalizados -->
-    <link href="../css/styles.css" rel="stylesheet">
+    <link href="../css/list_contacts.css" rel="stylesheet">
 </head>
 <body>
     <div class="container my-5">
